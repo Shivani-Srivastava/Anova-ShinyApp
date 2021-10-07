34e87291325981e15d5fee1c04cccb7edbda9eec
Shivani Srivastava <shivani141296@gmail.com>
2021-10-07 16:37:56 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,13 @@
 
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("pastecs")){install.packages("pastecs")}
+if(!require("foreign")){install.packages("foreign")}
+if(!require("tidyverse")){install.packages("tidyverse")}
+if(!require("psych")){install.packages("psych")}
+if(!require("ggpubr")){install.packages("ggpubr")}
+if(!require("ggplot2")){install.packages("ggplot2")}
+if(!require("DT")){install.packages("DT")}
+
 library(shiny)
 library(foreign)
 library(tidyverse)
@@ -6,6 +15,7 @@ library(psych)
 library(pastecs)
 library(tibble)
 library(ggpubr)
+library(ggplot2)
 
 shinyServer(function(input, output) {
     
